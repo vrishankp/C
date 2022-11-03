1449556663019355cd5fcfd017480ab3a2bc2d8d
Vrishank Paladugu <67123818+vrishankp@users.noreply.github.com>
2022-11-03 17:47:08 -0500

    Added prime.C
    This is the same prime.py from my other repo, but translated into C. I was testing how efficient C is compared to python and saw that when I was generating all the primes between 1 and 100,000, the C version was around 30 times faster.

---
+++ prime.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+    int lower = 1;
+
+    printf("Enter a number: ");
+    int upper;
+    scanf("%i", &upper);
+
+    int total = 0;
+
+    printf("Prime numbers between %d and %d are:\n", lower, upper);
+
+    clock_t start = clock();
+
+    int check = 0;
+
+    for (int num = lower; num < upper + 1; num++){
+        if (num > 1){
+            for (int i = 2; i < floor((num/2) + 1) ; i++){
+                if (num % i == 0){
+                    check = 1;
+                    break;
+                }
+            }
+
+            if (check == 0){
+                printf("%d\n", num);
+                total += 1;
+            } else {
+                check = 0;
+            }     
+        }
+    }
+
+    printf("Total number of primes in the range: %d\n", total );
+    clock_t exetime = clock() - start;
+    printf("Time it took: %Lf\n", (long double) ((long double) exetime / (double) CLOCKS_PER_SEC));
+    return 0;
+}
+
